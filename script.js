d44597602f21209197ebc2c8ac1a667414cb94f4
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-28 12:47:20 -0400

    complete quiz 8

--- script.js
+++ script.js
@@ -2,35 +2,41 @@
 
 const leftBtn = document.querySelector(".left");
 const rightBtn = document.querySelector(".right");
+const carouselNav = document.querySelector(".carousel-nav");
 
 const carouselItems = Array.from(document.querySelectorAll(".carousel-item"));
-const carouselNavItems = document.querySelectorAll(".nav-item");
+const carouselNavItems = Array.from(document.querySelectorAll(".nav-item"));
 const CAROUSEL_SIZE = carouselItems.length;
 
-leftBtn.addEventListener("click", swipe);
-rightBtn.addEventListener("click", swipe);
+leftBtn.addEventListener("click", clickNav);
+rightBtn.addEventListener("click", clickNav);
+carouselNav.addEventListener("click", clickNav);
 
-function swipe(e) {
+function clickNav(e) {
   const currentCarouselItem = document.querySelector(".carousel-item.active");
   const currentIndex = carouselItems.indexOf(currentCarouselItem);
   let nextIndex;
-
+  if (e.target.classList.contains("nav-item")) {
+    const chosenNav = e.target;
+    nextIndex = carouselNavItems.indexOf(chosenNav);
+  }
   if (e.currentTarget.classList.contains("left")) {
     if (currentIndex === 0) {
       nextIndex = CAROUSEL_SIZE - 1;
     } else {
       nextIndex = currentIndex - 1;
     }
-  } else {
+  } else if (e.currentTarget.classList.contains("right")) {
     if (currentIndex === CAROUSEL_SIZE - 1) {
       nextIndex = 0;
     } else {
       nextIndex = currentIndex + 1;
     }
   }
-
-  carouselItems[nextIndex].classList.add("active");
-  carouselNavItems[nextIndex].classList.add("active");
-  currentCarouselItem.classList.remove("active");
-  carouselNavItems[currentIndex].classList.remove("active");
+  if (nextIndex !== undefined) {
+    carouselItems[nextIndex].classList.add("active");
+    carouselNavItems[nextIndex].classList.add("active");
+    currentCarouselItem.classList.remove("active");
+    carouselNavItems[currentIndex].classList.remove("active");
+  }
 }
