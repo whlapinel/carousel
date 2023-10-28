62f05facfcc2e6c9998c2dec3f085a71d4108998
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-28 10:03:38 -0400

    Module 9 Quiz: Carousel

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+
+const carouselItems = Array.from(document.querySelectorAll(".carousel-item"));
+const carouselNavItems = document.querySelectorAll(".nav-item");
+const CAROUSEL_SIZE = carouselItems.length;
+
+leftBtn.addEventListener("click", swipe);
+rightBtn.addEventListener("click", swipe);
+
+function swipe(e) {
+  const currentCarouselItem = document.querySelector(".carousel-item.active");
+  const currentIndex = carouselItems.indexOf(currentCarouselItem);
+  let nextIndex;
+
+  if (e.currentTarget.classList.contains("left")) {
+    if (currentIndex === 0) {
+      nextIndex = CAROUSEL_SIZE - 1;
+    } else {
+      nextIndex = currentIndex - 1;
+    }
+  } else {
+    if (currentIndex === CAROUSEL_SIZE - 1) {
+      nextIndex = 0;
+    } else {
+      nextIndex = currentIndex + 1;
+    }
+  }
+
+  carouselItems[nextIndex].classList.add("active");
+  carouselNavItems[nextIndex].classList.add("active");
+  currentCarouselItem.classList.remove("active");
+  carouselNavItems[currentIndex].classList.remove("active");
+}
