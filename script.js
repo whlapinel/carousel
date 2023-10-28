da09ac3649b2280157c36ae5f2a76a3e3424fdf9
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-28 12:53:51 -0400

    fix issue with clicking on active nav-item

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict';
 
-const leftBtn = document.querySelector(".left");
-const rightBtn = document.querySelector(".right");
-const carouselNav = document.querySelector(".carousel-nav");
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
+const carouselNav = document.querySelector('.carousel-nav');
 
-const carouselItems = Array.from(document.querySelectorAll(".carousel-item"));
-const carouselNavItems = Array.from(document.querySelectorAll(".nav-item"));
+const carouselItems = Array.from(document.querySelectorAll('.carousel-item'));
+const carouselNavItems = Array.from(document.querySelectorAll('.nav-item'));
 const CAROUSEL_SIZE = carouselItems.length;
 
-leftBtn.addEventListener("click", clickNav);
-rightBtn.addEventListener("click", clickNav);
-carouselNav.addEventListener("click", clickNav);
+leftBtn.addEventListener('click', clickNav);
+rightBtn.addEventListener('click', clickNav);
+carouselNav.addEventListener('click', clickNav);
 
 function clickNav(e) {
-  const currentCarouselItem = document.querySelector(".carousel-item.active");
+  const currentCarouselItem = document.querySelector('.carousel-item.active');
   const currentIndex = carouselItems.indexOf(currentCarouselItem);
   let nextIndex;
-  if (e.target.classList.contains("nav-item")) {
+  if (e.target.classList.contains('nav-item')) {
     const chosenNav = e.target;
     nextIndex = carouselNavItems.indexOf(chosenNav);
   }
-  if (e.currentTarget.classList.contains("left")) {
+  if (e.currentTarget.classList.contains('left')) {
     if (currentIndex === 0) {
       nextIndex = CAROUSEL_SIZE - 1;
     } else {
       nextIndex = currentIndex - 1;
     }
-  } else if (e.currentTarget.classList.contains("right")) {
+  } else if (e.currentTarget.classList.contains('right')) {
     if (currentIndex === CAROUSEL_SIZE - 1) {
       nextIndex = 0;
     } else {
       nextIndex = currentIndex + 1;
     }
   }
-  if (nextIndex !== undefined) {
-    carouselItems[nextIndex].classList.add("active");
-    carouselNavItems[nextIndex].classList.add("active");
-    currentCarouselItem.classList.remove("active");
-    carouselNavItems[currentIndex].classList.remove("active");
+  if (nextIndex !== undefined && nextIndex !== currentIndex) {
+    carouselItems[nextIndex].classList.add('active');
+    carouselNavItems[nextIndex].classList.add('active');
+    currentCarouselItem.classList.remove('active');
+    carouselNavItems[currentIndex].classList.remove('active');
   }
 }
